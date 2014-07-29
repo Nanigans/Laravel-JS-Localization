3e3f45068e57d63d9b85e2f9687a006ad3eb6ce2
David Joy <davjoy@gmail.com>
2014-07-29 14:09:37 -0400

    Modifying JS template files so they work with require.js.
    1. Changing how it uses define() - I don't think the author combined define with a prototype class properly.
    2. Setting messages on the prototype instead of on the instance after it's created.

--- js/lang.js
+++ js/lang.js
@@ -1,28 +1,4 @@
-/*!
- *  Lang.js for Laravel localization in JavaScript.
- *
- *  @version 1.0.2
- *  @license MIT
- *  @site    https://github.com/rmariuzzo/Laravel-JS-Localization
- *  @author  rmariuzzo
- */
 
-'use strict';
-
-(function(root, factory) {
-
-    if (typeof define === 'function' && define.amd) {
-        // AMD support.
-        define([], factory);
-    } else if (typeof exports === 'object') {
-        // NodeJS support.
-        module.exports = new(factory())();
-    } else {
-        // Browser global support.
-        root.Lang = new(factory())();
-    }
-
-}(this, function() {
 
     // Default options //
 
@@ -250,6 +226,4 @@
         return false;
     };
 
-    return Lang;
 
-}));
--- src/Generators/Templates/langjs_with_messages.js
+++ src/Generators/Templates/langjs_with_messages.js
@@ -1,5 +1,33 @@
-'{ langjs }';
+/*!
+ *  Lang.js for Laravel localization in JavaScript.
+ *
+ *  @version 1.0.2
+ *  @license MIT
+ *  @site    https://github.com/rmariuzzo/Laravel-JS-Localization
+ *  @author  rmariuzzo
+ */
 
-(function(root) {
-    Lang.setMessages('{ messages }');
-})(window);
+'use strict';
+
+(function(root, factory) {
+
+  if (typeof define === 'function' && define.amd) {
+    // AMD support.
+    define([], new(factory())());
+  } else if (typeof exports === 'object') {
+    // NodeJS support.
+    module.exports = new(factory())();
+  } else {
+    // Browser global support.
+    root.Lang = new(factory())();
+  }
+
+}(this, function() {
+
+  '{ langjs }';
+
+  Lang.prototype.messages = '{ messages }';
+
+  return Lang;
+
+}));
